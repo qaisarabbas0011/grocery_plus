cd3ffc648c91d1d93ff75fbf93f03a003bdd114e
Qaisar Abbas <qaisarabbas5422@gmail.com>
2025-04-08 16:04:07 +0500

    second commit

--- grocery_app/lib/screens/home_screen.dart
+++ grocery_app/lib/screens/home_screen.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:grocery_plus/widgets/custom_text_field.dart';
+import 'package:grocery_plus/widgets/location_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var searchController = TextEditingController();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home"),
+        body: SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 20.0),
+        child: Column(children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Grocery Plus",
+                style: GoogleFonts.poppins(
+                    fontSize: 20, fontWeight: FontWeight.w600),
+              ),
+              Icon(Icons.notifications_outlined)
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          LocationWidget(),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.02,
+          ),
+          CustomTextField(
+              hintText: "Search here",
+              prefixIcon: Icon(Icons.search),
+              controller: searchController)
+        ]),
       ),
-    );
+    ));
   }
 }
